3ba3c7cb902dded02652c223301a7df235d5b804
Tayabur Rahman <tayabur.rahman@niftycoders.com>
2025-12-22 11:12:46 +0600

    environment file added

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class NewsService {
-  private newsUrl = 'http://localhost:3000/news';
+  private newsUrl = `${environment.apiUrl}/news`;
 
   constructor(private http: HttpClient) { }
 
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class UsersService {
-  private usersUrl = 'http://localhost:3000/users';
+  private usersUrl = `${environment.apiUrl}/users`;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getAll(): Observable<any[]> { return this.http.get<any[]>(this.usersUrl); }
   getById(id: number): Observable<any> { return this.http.get<any>(`${this.usersUrl}/${id}`); }
